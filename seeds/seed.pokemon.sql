47334a70334cfc5b9db00680c5f16745f1c53b2b
dasha akhten <dasha.akhten@gmail.com>
2021-07-17 11:02:26 -0700

    feat: tables

---
+++ seeds/seed.pokemon.sql
@@ -0,0 +1,5 @@
+INSERT INTO pokemon (id, title,superpower, age )
+VALUES
+    (1, 'pickachu', 'yellow', 'two'),
+    (2, 'jigglypuff', 'super cute', 'three'),
+    (3, 'a blue one', 'never ages', 'four');
